dc1511005b0f51061f58a5b9f6b190e44655efb1
jakubasz <92277382+jakubasz@users.noreply.github.com>
2022-11-01 19:22:27 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,46 @@
 
 #include "Resource.hpp"
 
+using namespace std;
+
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+
+public:
+    Resource* wskaznik;
+
+    ResourceManager() : wskaznik(new Resource) {}
+    ~ResourceManager() { delete wskaznik; }
+
+    double get() { return wskaznik->get(); }
+
+    ResourceManager(const ResourceManager& r) : wskaznik(r.wskaznik)
+    {
+        wskaznik = new Resource(*r.wskaznik);
+    }
+
+    ResourceManager& operator=(const ResourceManager& r)
+    {
+        if (wskaznik == r.wskaznik)
+            return *this;
+        delete wskaznik;
+        wskaznik = new Resource(*r.wskaznik);
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& r) : wskaznik(r.wskaznik)
+    {
+        wskaznik   = r.wskaznik;
+        r.wskaznik = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& r)
+    {
+        if (wskaznik == r.wskaznik)
+            return *this;
+        delete wskaznik;
+        wskaznik   = r.wskaznik;
+        r.wskaznik = nullptr;
+        return *this;
+    };
 };
